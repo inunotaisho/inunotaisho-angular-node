7dabb13bd5a970923e308c83fd034b1160f2b222
Ethan Fairweather <Ethan-Fairweather@Inunotaishos-MacBook-Pro.local>
2017-07-27 19:25:07 -0400

    remove files

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,60 +1,16 @@
-const fs = require('fs');
-const path = require('path');
-const ProgressPlugin = require('webpack/lib/ProgressPlugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const autoprefixer = require('autoprefixer');
-
-const { NoEmitOnErrorsPlugin, SourceMapDevToolPlugin, NamedModulesPlugin } = require('webpack');
+const webpack = require('webpack');
 const { CommonsChunkPlugin } = require('webpack').optimize;
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-const nodeModules = path.join(process.cwd(), 'node_modules');
-const realNodeModules = fs.realpathSync(nodeModules);
-const genDirNodeModules = path.join(process.cwd(), 'src', '$$_gendir', 'node_modules');
-const entryPoints = ["inline","polyfills","sw-register","vendor","main"];
-const minimizeCss = false;
-const baseHref = "";
-const deployUrl = "";
-const postcssPlugins = function () {
-        // safe settings based on: https://github.com/ben-eb/cssnano/issues/358#issuecomment-283696193
-        const importantCommentRe = /@preserve|@license|[@#]\s*source(?:Mapping)?URL|^!/i;
-        const minimizeOptions = {
-            autoprefixer: false,
-            safe: true,
-            mergeLonghand: false,
-            discardComments: { remove: (comment) => !importantCommentRe.test(comment) }
-        };
-        return [
-            postcssUrl({
-                url: (URL) => {
-                    // Only convert root relative URLs, which CSS-Loader won't process into require().
-                    if (!URL.startsWith('/') || URL.startsWith('//')) {
-                        return URL;
-                    }
-                    if (deployUrl.match(/:\/\//)) {
-                        // If deployUrl contains a scheme, ignore baseHref use deployUrl as is.
-                        return `${deployUrl.replace(/\/$/, '')}${URL}`;
-                    }
-                    else if (baseHref.match(/:\/\//)) {
-                        // If baseHref contains a scheme, include it as is.
-                        return baseHref.replace(/\/$/, '') +
-                            `/${deployUrl}/${URL}`.replace(/\/\/+/g, '/');
-                    }
-                    else {
-                        // Join together base-href, deploy-url and the original URL.
-                        // Also dedupe multiple slashes into single ones.
-                        return `/${baseHref}/${deployUrl}/${URL}`.replace(/\/\/+/g, '/');
-                    }
-                }
-            }),
-            autoprefixer(),
-        ].concat(minimizeCss ? [cssnano(minimizeOptions)] : []);
-    };
-
-
-
+const VENDOR_LIBS =[
+    '@angular/animations', '@angular/common', '@angular/compiler', '@angular/core', '@angular/forms',
+    '@angular/http', '@angular/platform-browser', '@angular/platform-browser-dynamic', '@angular/router', 'core-js', 
+    'es6-shim', 'platypus','platypusui', 'reflect-metadata','rxjs', 'zone.js'
+]
 
 module.exports = {
-  "resolve": {
+    devtool: 'source-map',
+    "resolve": {
     "extensions": [
       ".ts",
       ".js"
@@ -64,102 +20,53 @@ module.exports = {
       "./node_modules"
     ],
     "symlinks": true
-  },
-  "resolveLoader": {
-    "modules": [
-      "./node_modules",
-      "./node_modules"
-    ]
-  },
-  "entry": {
-    "main": [
-      "./src/main.ts"
+    },
+    entry: {
+       main:"./src/main.ts",
+       polyfills:"./src/polyfills.ts",
+       vendor: VENDOR_LIBS
+    },
+    target: 'web',
+    output: {
+        path: `${__dirname}/public/dist/`,
+        filename: '[name].bundle.js'
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify('development'),
+            __DEV__: true
+        }),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoEmitOnErrorsPlugin(),
+        new CommonsChunkPlugin({
+        "name": [
+            "inline"
+        ],
+        "minChunks": null
+    }),
     ],
-    "polyfills": [
-      "./src/polyfills.ts"
-    ]
-  },
-  "output": {
-    "path": `${__dirname}/public/dist/`,
-    "filename": "[name].bundle.js",
-    "chunkFilename": "[id].chunk.js"
-  },
-  "module": {
-    "rules": [
-      {
+    module:{
+        rules: [
+            {
         "enforce": "pre",
         "test": /\.js$/,
         "loader": "source-map-loader",
         "exclude": [
           /\/node_modules\//
         ]
-      },
-      {
+        }, {
         "test": /\.json$/,
         "loader": "json-loader"
-      },
-      {
-        "test": /\.html$/,
-        "loader": "raw-loader"
-      },
-      {
-        "test": /\.(eot|svg)$/,
-        "loader": "file-loader?name=[name].[hash:20].[ext]"
-      },
-      {
-        "test": /\.(jpg|png|webp|gif|otf|ttf|woff|woff2|cur|ani)$/,
-        "loader": "url-loader?name=[name].[hash:20].[ext]&limit=10000"
-      },
-      {
-        "test": /\.ts$/,
-        "loader": "ts-loader"
-      }
-    ]
-  },
-  "plugins": [
-    new NoEmitOnErrorsPlugin(),
-    new ProgressPlugin(),
-    new CommonsChunkPlugin({
-      "minChunks": 2,
-      "async": "common"
-    }),
-    new CommonsChunkPlugin({
-      "name": [
-        "inline"
-      ],
-      "minChunks": null
-    }),
-    new CommonsChunkPlugin({
-      "name": [
-        "vendor"
-      ],
-      "minChunks": (module) => {
-                return module.resource
-                    && (module.resource.startsWith(nodeModules)
-                        || module.resource.startsWith(genDirNodeModules)
-                        || module.resource.startsWith(realNodeModules));
-            },
-      "chunks": [
-        "main"
-      ]
-    }),
-    new SourceMapDevToolPlugin({
-      "filename": "[file].map[query]",
-      "moduleFilenameTemplate": "[resource-path]",
-      "fallbackModuleFilenameTemplate": "[resource-path]?[hash]",
-      "sourceRoot": "webpack:///"
-    }),
-    new NamedModulesPlugin({})
-  ],
-  "node": {
-    "fs": "empty",
-    "global": true,
-    "crypto": "empty",
-    "tls": "empty",
-    "net": "empty",
-    "process": true,
-    "module": false,
-    "clearImmediate": false,
-    "setImmediate": false
-  }
-};
+        }, {
+            test: /\.html$/,
+            use: [{
+                loader: 'raw-loader'
+            }],
+        }, {
+            test: /\.ts$/, 
+            use: [{
+                loader: 'ts-loader'
+            }]
+        }]
+    }
+}
