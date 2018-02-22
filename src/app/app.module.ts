c79a0393973e26c024ce2aacc6bc16675cb05e9a
inunotaisho26 <ebfairweather@gmail.com>
2018-02-22 17:07:58 -0500

    moving app module update to github

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,48 +9,18 @@ import { FroalaEditorModule, FroalaViewModule } from 'angular-froala-wysiwyg';
 
 import { Navbar} from './views/navbar/navbar.component';
 
-import { HomeComponent } from './views/home/home.component';
-import { EdComponent } from './views/education/ed.component';
-import { PortfolioComponent } from './views/portfolio/portfolio.component';
-import { BlogComponent } from './views/blog/blog.component';
-import { BlogPostComponent } from './views/blogpost/blogpost.component';
-import { LoginComponent }from './views/login/login.component';
-import { ProfileComponent } from './views/profile/profile.component';
-import { WriteComponent } from './views/write/write.component';
-import { ContactComponent } from './views/contact/contact.component';
-import { RegComponent } from './views/reg/reg.component';
 import portImgContainer from './views/portfolio/images/portfolio.image.component';
 
 import { AuthService } from './views/service/authentication.service';
 
-export const routes: Routes = [
- {path:'', pathMatch:'full', component:HomeComponent},
- {path:'education', component:EdComponent},
- {path:'portfolio', component: PortfolioComponent},
- {path:'blog', component: BlogComponent},
- {path:'blogPost', component: BlogPostComponent},
- {path:'login', component: LoginComponent},
- {path:'profile', component: ProfileComponent},
- {path:'write', component: WriteComponent},
- {path:'contact', component: ContactComponent},
- {path:'reg', component: RegComponent}
-]
+import { routes } from './app.route';
 
 export function declarations(): any {
     return [
         AppComponent,
-        HomeComponent,
-        EdComponent,
-        PortfolioComponent,
-        portImgContainer,
-        BlogComponent,
-        BlogPostComponent,
-        LoginComponent,
-        ProfileComponent,
-        WriteComponent,
-        ContactComponent,
-        RegComponent,
-        Navbar
+        routes,
+        Navbar,
+        portImgContainer
     ]
 }
 
