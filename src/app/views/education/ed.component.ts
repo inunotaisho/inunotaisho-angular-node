b25cb881717d9ef13d8e7565a4467de1be9061f4
inunotaisho26 <ebfairweather@gmail.com>
2018-02-22 23:39:09 -0500

    removing double reference

--- src/app/views/education/ed.component.ts
+++ src/app/views/education/ed.component.ts
@@ -2,8 +2,6 @@ import {Component, OnInit} from '@angular/core';
 import { Http } from '@angular/http';
 //var edComponent = require('./ed.component.html');
 
-//var edComponent = require('./ed.component.html');
-
 
 @Component({
     templateUrl:'./ed.component.html' //template: edComponent
