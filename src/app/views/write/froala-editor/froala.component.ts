06666a822434d05e8e2383b2fe6be31d976ab454
inunotaisho26 <ebfairweather@gmail.com>
2018-03-01 16:26:29 -0500

    creating froala editor component

---
+++ src/app/views/write/froala-editor/froala.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit  } from '@angular/core';
+declare var $ :any;
+
+@Component({
+    selector:'froala-editor',
+    templateUrl:'./frola.component.html' //template: portImgComponent
+})
+
+export default class froalaEditor implements OnInit {
+    ngOnInit(){
+        $.FroalaEditor({
+            // Set the image upload parameter.
+            imageUploadUrl: '',
+
+            // Set request type.
+            imageUploadMethod: 'POST',
+
+            // Allow to upload PNG and JPG.
+            imageAllowedTypes: ['jpeg', 'jpg', 'png']
+        })
+        .on
+    };
+}
