6ff931298bcf90a0f2a1999b80c16813f50aa652
inunotaisho26 <ebfairweather@gmail.com>
2018-03-19 12:18:40 -0400

    adding froala changes

--- src/app/views/write/froala-editor/froala.component.ts
+++ src/app/views/write/froala-editor/froala.component.ts
@@ -1,15 +1,48 @@
-import { Component, OnInit  } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter  } from '@angular/core';
+import { FroalaEditorModule } from 'angular-froala-wysiwyg';
 declare var $ :any;
+import * as Cloudinary from '@cloudinary/angular';
 
 @Component({
     selector:'froala-editor',
-    templateUrl:'./frola.component.html' //template: portImgComponent
+    templateUrl:'./froala.component.html' //template: portImgComponent
 })
 
 export default class FroalaEditor implements OnInit {
+    public editorOptions: any;
+
+    @Input()
+    public  post: string;
+
+    @Output()
+    public onChange: EventEmitter<any> = new EventEmitter();
+
     ngOnInit(){
-        $.FroalaEditor(function (e:any){
-            e.preventDefault;
-        })
+        this.editorOptions = {
+            placeholderText: 'Enter Blog Post Here',
+            events: {
+                'froalaEditor.initialized': function(editor) {
+                    console.log('Here editor initialized', editor);
+                }, 
+                'froalaEditor.image.beforeUpload': (e, editor, images) => {
+                    console.log(images);
+
+                    //@TODO heee we save the images to cloudinary
+                },
+                'froalaEditor.file.beforeUpload': (e, editor, files) => {
+
+                }
+            },
+            options: {
+                fileUploadToS3: {
+                    
+                }
+            }
+
+        }
     };
+
+    modelChange(e) {
+        this.onChange.emit(e);
+    }
 }
