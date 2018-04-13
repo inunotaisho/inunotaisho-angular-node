6dec0a8df6f52d292f5b79fbe50797180b06e7d0
inunotaisho26 <ebfairweather@gmail.com>
2018-04-13 19:33:37 -0400

    changing from session to token

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose"),
+      jwt = require('jsonwebtoken'),
       crypto=require('crypto');
 
 const Schema = mongoose.Schema;
@@ -22,6 +23,17 @@ UserSchema.methods.setPassword = function(password) {
 UserSchema.methods.comparePassword = function (password) {
   const hash = crypto.pbkdf2Sync(password, this.Salt, 10000, 512,'sha512').toString('hex')
   return this.Hash === hash;
+}
+
+UserSchema.methods.generateJWT = function () {
+  const today = new Date();
+  const exp = new Date(today);
+  exp.setDate(today.getDate() + 5);
+  return jwt.sign({
+      id: this._id,
+      Username: this.Username,
+      exp: parseInt(exp.getTime() / 1000)
+  }, process.env.SECRET)
 }
 
 const UserModel = mongoose.model('User', UserSchema);
