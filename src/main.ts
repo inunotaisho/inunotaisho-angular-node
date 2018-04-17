3b40296c805d996a874f41899592c07bd878beb0
inunotaisho26 <ebfairweather@gmail.com>
2018-04-16 21:54:39 -0400

    updating main ts

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
+import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { enableProdMode, isDevMode } from '@angular/core';
+
+
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment'
 
@@ -7,4 +9,5 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.log(err));
