290af48af288f614160e4c9f0d4997492ddab693
inunotaisho26 <ebfairweather@gmail.com>
2018-05-07 10:53:47 -0400

    changing paths from views to components

--- src/app/routes/routes.module.ts
+++ src/app/routes/routes.module.ts
@@ -1,17 +1,17 @@
 import { Routes, CanActivate } from '@angular/router';
 
-import { HomeComponent } from '../views/home/home.component';
-import { EdComponent } from '../views/education/ed.component';
-import { PortfolioComponent } from '../views/portfolio/portfolio.component';
-import { BlogComponent } from '../views/blog/blog.component';
-import { BlogPostComponent } from '../views/blogpost/blogpost.component';
-import { LoginComponent }from '../views/login/login.component';
-import { ProfileComponent } from '../views/profile/profile.component';
-import { WriteComponent } from '../views/write/write.component';
-import { ContactComponent } from '../views/contact/contact.component';
-import { RegComponent } from '../views/reg/reg.component';
-import portImgContainer from '../views/portfolio/images/portfolio.image.component';
-import FroalaEditor from '../views/write/froala-editor/froala.component';
+import { HomeComponent } from '../components/home/home.component';
+import { EdComponent } from '../components/education/ed.component';
+import { PortfolioComponent } from '../components/portfolio/portfolio.component';
+import { BlogComponent } from '../components/blog/blog.component';
+import { BlogPostComponent } from '../components/blogpost/blogpost.component';
+import { LoginComponent }from '../components/login/login.component';
+import { ProfileComponent } from '../components/profile/profile.component';
+import { WriteComponent } from '../components/write/write.component';
+import { ContactComponent } from '../components/contact/contact.component';
+import { RegComponent } from '../components/reg/reg.component';
+import portImgContainer from '../components/portfolio/images/portfolio.image.component';
+import FroalaEditor from '../components/write/froala-editor/froala.component';
 
 import { AuthGuard } from "../services/authguard/authguard.service"
 
