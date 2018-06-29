4203e0ad3983f8039b99b8f5b692ffed57d02640
inunotaisho26 <ebfairweather@gmail.com>
2018-06-29 13:44:45 -0400

    adding social banner to common folder

---
+++ src/app/common/banners/social/social.component.ts
@@ -0,0 +1,14 @@
+import {Component, OnInit} from '@angular/core';
+
+
+@Component({
+    selector:'social-component',
+    templateUrl:'./social.component.html' //template: portImgComponent
+})
+
+export default class socialBannerComponent {
+    constructor(){
+
+    }
+
+}
