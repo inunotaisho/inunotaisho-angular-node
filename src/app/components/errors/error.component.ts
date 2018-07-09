abff8942d479b20f254572dd079361a86c2f37cb
inunotaisho26 <ebfairweather@gmail.com>
2018-07-09 19:39:45 -0400

    updating error component interpolate english json

--- src/app/components/errors/error.component.ts
+++ src/app/components/errors/error.component.ts
@@ -1,11 +1,22 @@
-import {Component} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '../../services/translate/translate.service';
 
 @Component({
     templateUrl:'./error.component.html' //template: loginComponent
 })
 
-export class ErrorComponent {
+export class ErrorComponent implements OnInit{
 
+    data: any = {};
 
+    constructor( private translateService: TranslateService ){
+
+    }
+
+    ngOnInit(){
+        this.translateService.getJSON().subscribe(data => {
+             this.data = data;
+         });
+    }
     
 }
