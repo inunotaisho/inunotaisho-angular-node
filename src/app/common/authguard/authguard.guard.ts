e208ce6db0600ec9b7be39b4fad6f7fffd06cbe7
inunotaisho26 <ebfairweather@gmail.com>
2018-08-04 16:00:31 -0400

    updating authguard

--- src/app/common/authguard/authguard.guard.ts
+++ src/app/common/authguard/authguard.guard.ts
@@ -1,17 +1,39 @@
 import { Injectable} from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
+import { CanActivate, Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 
 import {AuthService} from '../../services/authservice/authentication.service';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-    constructor(private auth: AuthService, private router:Router){
+    constructor(private authService: AuthService, private router:Router){
 
     }
-    canActivate(){
-        if(!this.auth.isLoggedIn){
-            this.router.navigate(["login"])
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot){
+        if(this.authService.isLoggedIn){
+            return true
         }
-        return this.auth.isLoggedIn;
+
+        this.authService.redirectUrl = state.url;
+        this.router.navigate(["login"])
     }
+}
+
+/**
+ * If the user is already logged in, and attempts to navigate to /login, then redirect them to home.
+ */
+
+@Injectable()
+export class alreadyLoggedIn implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) {
+
+  }
+
+  canActivate(): boolean {
+    if (this.authService.isLoggedIn) {
+      this.router.navigate(['/']);
+      return false;
+    }
+
+    return true;
+  }
 }
