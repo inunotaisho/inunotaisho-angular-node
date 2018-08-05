649f9867ce7ac37ebbd99918185f50995cfc5e47
inunotaisho26 <ebfairweather@gmail.com>
2018-08-05 14:42:21 -0400

    activating the return button

--- src/app/common/banners/returnbutton/returnbutton.component.ts
+++ src/app/common/banners/returnbutton/returnbutton.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'return-button',
@@ -9,6 +9,37 @@ export class ReturnButtonComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  // When the user scrolls down 20px from the top of the document, show the button
+  @HostListener('window: scroll', ['$event'])
+  scroll($event) { 
+    console.log('here ')
+    this.scrollFunction();
+  };
+
+  scrollFunction() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+      console.log('scrolled')
+      document.getElementById("scrollup").style.display = "block";
+      // document.getElementById("scrollup").style.visibility = "visible";
+      // document.getElementById("scrollup").style.opacity = "1";
+      // document.getElementById("scrollup").style.transition = "opacity 2s linear";
+      
+    } else {
+      console.log('not scrolled')
+      document.getElementById("scrollup").style.display = "none";
+
+  //     visibility: hidden;
+  // opacity: 0;
+  // transition: visibility 0s 2s, opacity 2s linear;
+    }
+  }
+
+  // When the user clicks on the button, scroll to the top of the document
+  topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
   }
 
 }
