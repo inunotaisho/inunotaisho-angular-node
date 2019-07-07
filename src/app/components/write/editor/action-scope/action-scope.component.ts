6709ab4187d216d43c4af1935c9ad3c0287861e9
inunotaisho26 <ebfairweather@gmail.com>
2019-07-07 10:46:32 -0400

    feature(create): creating component for editors inline toolbar.

---
+++ src/app/components/write/editor/action-scope/action-scope.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-action-scope',
+  templateUrl: './action-scope.component.html'
+})
+export class ActionScopeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
