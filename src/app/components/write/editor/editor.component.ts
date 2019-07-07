3c9b8f8b89512f446febf92caaa3db7cf61da3ea
inunotaisho26 <ebfairweather@gmail.com>
2019-07-07 12:22:21 -0400

    upgrade(): adding custom selectors and cleaning up html

--- src/app/components/write/editor/editor.component.ts
+++ src/app/components/write/editor/editor.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, Input, Output, EventEmitter, HostListener, NgZone } from '@angular/core';
-import { AppSettings } from './../../../common/config';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from './../../../services/authservice/authentication.service';
+import { MediaToolTipComponent } from './media-tool-tip/media-tool-tip.component';
 import { ListenerDefaults, Combo, Listener } from 'keypress.js';
 import { json } from 'body-parser';
 
@@ -10,17 +11,23 @@ import { json } from 'body-parser';
 })
 
 export default class EditorComponent implements OnInit {
-    content: string
-    image: string
-    title:string
+
+
+    title:string;
+    author: string;
+    content: string;
+    published: Date;
+
 
     constructor(
-        private auth: AuthService
-    ) { }
+        private auth: AuthService,
+        private http: HttpClient
+    ) {
+    }
 
     blogs = []
 
-    ngOnInit() {
+    ngOnInit(){
 
     }
 
@@ -30,31 +37,8 @@ export default class EditorComponent implements OnInit {
             author: this.auth.getUsername(),
             // authorId: this.auth
             content: this.content,
-            image: this.image || null,
             published: new Date()
         }
 
     }
-    // When the user clicks on button, it opens and close the tooltip menu
-    // @HostListener('window: toolTipMenu', ['$event'])
-    // toolTip($event){
-
-    // }
-
-    // toolTipMenu($event){
-    //     if () {
-    //         document.getElementById("tooltipbutton").classList.add('is-scaled');
-    //     } else {
-    //         document.getElementById("tooltipbutton").classList.remove('is-scaled');
-    //     }
-    // }
-
-    // textAdded($event){
-    //     if () {
-    //         document.getElementById("tooltipbutton").classList.remove('is-active');
-    //     } else {
-    //         document.getElementById("tooltipbutton").classList.add('is-active');
-    //     }
-    // }
-
 }
