cc4c874cab2eb3d8a23308795c4402a188e452b4
inunotaisho26 <ebfairweather@gmail.com>
2019-07-07 10:35:30 -0400

    feature(create): creating component for to input reply

---
+++ src/app/components/blogPost/master-footer/response-editor/response-editor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-response-editor',
+  templateUrl: './response-editor.component.html',
+  styleUrls: ['./response-editor.component.css']
+})
+export class ResponseEditorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
