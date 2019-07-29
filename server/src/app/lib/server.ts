3661def1eeb04534bfb1bf991f6f9f7ba0d5499b
inunotaisho26 <ebfairweather@gmail.com>
2019-07-29 16:05:17 -0400

    template(): to be used later

--- server/src/app/lib/server.ts
+++ server/src/app/lib/server.ts
@@ -7,28 +7,42 @@ import * as helmet from 'helmet';
 import * as favicon from 'serve-favicon';
 import * as methodOverride from 'method-override';
 import * as morgan from 'morgan';
-import { createServer, Server } from "http";
 import * as https from 'https';
+import { ROUTER } from '../routes/routes';
+// import { Connection } from './Database';
 
 
 
 
-export class app {
+export class Application {
 
-  public app: express.Application;
+  private static async ConnectDB(): Promise<any> {
+    const connection = await Connection;
 
-  public Start(): Promise<express.Application> {
+    try {
+      await connection.synchronize();
+    } catch (e) {
+      console.log(e);
+      console.log("Couldn't synchronize database");
+    }
 
-    return new Promise((resolve, reject) => {
-      resolve(this.app);
-    });
-
+    return connection;
   }
+
+  private readonly app: express.Application;
+  private readonly server: https.Server;
 
   constructor() {
     this.app = express();
     this.config();
+    this.ConfigRouter();
 
+  }
+
+  public async Start(): Promise<express.Application> {
+    return new Promise((resolve, reject) => {
+      resolve(this.app);
+    })
   }
 
   private config() {
@@ -40,13 +54,73 @@ export class app {
     this.app.use(favicon(path.join(__dirname, '/assets', 'inu.ico')));
 
     // to extract form data from POST bodies
-    this.app.use(bodyParser.json());                         // for parsing application/json
+    this.app.use(bodyParser.json({ limit: '50mb' }));                         // for parsing application/json
     this.app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
     // serving static files 
     this.app.use(express.static('public'));
 
-    //requiring routes module
-    this.app.use(require('./routes/routes'));
+    this.app.use(
+      (req, res, next): void => {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Authorization');
+        res.header('Access-Control-Allow-Methods', 'GET,PUT,PATCH,POST,DELETE,OPTIONS');
+        next();
+      }
+    );
+
+    this.app.use(morgan('combined'));
+    this.app.use(
+      cors({
+        credentials: true,
+        origin: process.env.FRONT_URL || 'http://localhost:3000',
+      })
+    );
+
+    this.app.use(
+      (err: any, req: express.Request, res: express.Response, next: express.NextFunction): void => {
+        err.status = 404;
+        next(err);
+      }
+    );
+  }
+
+  private ConfigRouter(): void {
+    for (const route of ROUTER) {
+      this.app.use(route.path, route.middleware, route.handler);
+    }
+
+    this.app.use(
+      (req: express.Request, res: express.Response, next: express.NextFunction): void => {
+        res.status(404);
+        res.json({
+          error: 'Not found',
+        });
+        next();
+      }
+    );
+
+    this.app.use(
+      (err: any, req: express.Request, res: express.Response, next: express.NextFunction): void => {
+        if (err.name === 'UnauthorizedError') {
+          res.status(401).json({
+            error: 'Please send a valid Token...',
+          });
+        }
+        next();
+      }
+    );
+
+    this.app.use(
+      (err: any, req: express.Request, res: express.Response, next: express.NextFunction): void => {
+        res.status(err.status || 500);
+        res.json({
+          error: err.message,
+        });
+        next();
+      }
+    );
   }
 }
+
+export { Application as app }
