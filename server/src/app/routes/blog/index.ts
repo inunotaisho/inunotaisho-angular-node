e8e90881836b5c1df0cb3f7bd006bc9ce073a57b
inunotaisho26 <ebfairweather@gmail.com>
2019-09-15 17:53:52 -0400

    chore(): removing ts for example

--- server/src/app/routes/blog/index.ts
+++
@@ -1,5 +0,0 @@
-export { BlogGet } from './blogGet';
-export { BlogPost } from './blogPost';
-export { BlogPut } from './blogPut';
-export { BlogDelete } from './blogDelete';
-export { FileUpload } from './blogFile';
